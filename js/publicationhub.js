a1becd0c7d9c9a54349f1106bc9371d51a830c49
Tapestry Labs <qwiklit@gmail.com>
2017-03-01 16:18:17 -0500

    Added Canadian mapping database Finished Raw XML files for America

---
+++ js/publicationhub.js
@@ -0,0 +1,24 @@
+ jQuery(document).ready(function($){
+        
+        
+      //store publication names
+     
+      var pubNames = [
+          
+            {
+              "searchstring": "",
+              "pub" : "",
+            }, 
+           {
+              "searchstring": "",
+              "pub" : "",
+            }, {
+              "searchstring": "",
+              "pub" : "",
+            }, {
+              "searchstring": "",
+              "pub" : "",
+            },
+          
+      ];
+      
